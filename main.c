039d2a5d6f6183bf5a949f0a62d7dcc38f6b9983
NLKRUTHI <157903249+NLKRUTHI@users.noreply.github.com>
2024-07-30 15:12:12 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,49 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#define SIZE 100
 int count;
-int BFSM(char T[20],char P[10])
+void bubble_sort(int A[SIZE], int n)
 {
-    int n,m,i,j;
-    n = strlen(T);
-    m = strlen(P);
-    for(i=0;i<=n-m;i++)
+    int i , j , temp;
+    for(i=0;i<=n-2;i++)
     {
-        j=0;
-        while(j<m && P[j]==T[i+j])
+        for(j=0;j<=n-2-i;j++)
         {
-
-
-                j=j+1;
-                count++;
+            count++;
+            if(A[j]>A[j+1])
+            {
+                temp=A[j];
+                A[j]=A[j+1];
+                A[j+1]=temp;
+            }
         }
-       count++;
-       if(j==m)
-         return i;
     }
-    return -1;
 }
 int main()
 {
-    char T[20], P[10];
-    int flag;
-    printf("\n read a text:\n");
-    scanf("%s",T);
-    printf("\n read a pattern:\n");
-    scanf("%s",P);
-    flag=BFSM(T,P);
-    if(flag==-1)
-    {
-
-
-       printf("pattern not found \n");
-      printf("\n the number of comparison is:%d \n",count);
-    }
-    else{
-
-
-        printf("pattern found %d location \n",flag+1);
-        printf("\n the number of comparison is:%d \n",count-1);
-    }
-    return 0;
+    int i,n,A[SIZE];
+    printf("enter the size of the array:\n");
+    scanf("%d",&n);
+    printf("enter the elements:\n");
+    for(i=0;i<n;i++)
+     scanf("%d",&A[i]);
+    bubble_sort(A,n);
+    printf("\n enter the elements are:\n");
+    for(i=0;i<n;i++)
+        printf("%d\t",A[i]);
+    printf("\n the number of comparison is :%d \n",count);
 }
